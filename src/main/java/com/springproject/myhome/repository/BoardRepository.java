b6412c41b638a824dc891b62c66bc5fb2b608403
Jeongm1n <qll4359@naver.com>
2021-01-06 21:20:18 +0900

    Update CRUD

--- src/main/java/com/springproject/myhome/repository/BoardRepository.java
+++ src/main/java/com/springproject/myhome/repository/BoardRepository.java
@@ -3,6 +3,11 @@ package com.springproject.myhome.repository;
 import com.springproject.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // <> 안에는 model/Board 파일 명
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
