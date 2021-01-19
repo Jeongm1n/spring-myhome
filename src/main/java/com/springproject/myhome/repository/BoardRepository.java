8d81d6287a5cec803891f8aca8239a7e9311e51e
Jeongm1n <qll4359@naver.com>
2021-01-19 15:36:25 +0900

    Update Search function

--- src/main/java/com/springproject/myhome/repository/BoardRepository.java
+++ src/main/java/com/springproject/myhome/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.springproject.myhome.repository;
 
 import com.springproject.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title, String content);
+
+    // 검색기능을 위한
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
