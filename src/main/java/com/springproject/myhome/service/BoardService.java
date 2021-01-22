86a05178ab406aa043ff637300d53963e16f09e7
Jeongm1n <qll4359@naver.com>
2021-01-22 16:03:13 +0900

    Update OneToMany, ManyToOne in borad/user

---
+++ src/main/java/com/springproject/myhome/service/BoardService.java
@@ -0,0 +1,24 @@
+package com.springproject.myhome.service;
+
+import com.springproject.myhome.model.Board;
+import com.springproject.myhome.model.User;
+import com.springproject.myhome.repository.BoardRepository;
+import com.springproject.myhome.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Board save(String username, Board board) {
+        User user = userRepository.findByUsername(username);
+        board.setUser(user);
+        return boardRepository.save(board);
+    }
+}
